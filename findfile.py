6756bd096c32a8958004050ec0bb1f1885274e5b
Andrea Imparato <andrea@shellrent.com>
2015-05-21 12:06:56 +0200

    optimization

--- findfile.py
+++ findfile.py
@@ -14,34 +14,34 @@ destination_folder = sys.argv[2]
 destination_ext = sys.argv[3]
 
 files_list = []
-files_list_copy = []
 
 for root, dirs, files in os.walk(source_folder):
   for f in files:
     files_list.append(f)
-    files_list_copy.append(f)
 
+for root, dirs, files in os.walk(source_folder):
+  for f in files:
+    position_dot = f.rfind('.')
+    target_file = f[:position_dot] +'.'+ destination_ext
+    found = False
+    for destination_root, destination_dirs, destination_files in os.walk(destination_folder,topdown=True, onerror=None, followlinks=True):
+      for destination_file in destination_files:
+        if destination_file == target_file:
+          shutil.copy(destination_root + "/" + target_file, source_folder)
+          files_list.remove(f)
+          found = True
+          break
+        
+    print f
+    if found == False:
+      print "not found"
+    else:
+      print "found"
 
-for idf,f in enumerate(files_list):
-  position_dot = f.rfind('.')
-  target_file = f[:position_dot] +'.'+ destination_ext
-  found = False
-  for destination_root, destination_dirs, destination_files in os.walk(destination_folder,topdown=True, onerror=None, followlinks=True):
-    for destination_file in destination_files:
-      if destination_file == target_file:
-        shutil.copy(destination_root + "/" + target_file, source_folder)
-        files_list_copy.remove(f)
-        found = True
-  print f
-  if found == False:
-    print "found"
-  else:
-    print "not found"
-
-if len(files_list_copy) > 0:
+if len(files_list) > 0:
   print "\n"
   print "files not found:"
-  for f in files_list_copy:
+  for f in files_list:
     print f
 else:
   print "all files have been copied"
